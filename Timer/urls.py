3ece828980f535b9212d9b33c15827ef79c51043
Coded.om <137064980+coded-om@users.noreply.github.com>
2023-08-20 23:26:12 +0400

    Add files via upload

---
+++ Timer/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path("", view=views.index, name="index"),
+    # path("user_info.html", view=views.user_info, name="user_info"),
+    # path("boking.html", view=views.boking, name="boking"),
+    # path("bill.html", view=views.bill, name="pyment"),
+]
